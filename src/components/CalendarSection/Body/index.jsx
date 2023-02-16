54c4dd796a2bcd76dfffc5833e8b1c2099292fec
letsjo <hyunoh.jo@gmail.com>
2023-02-17 03:43:51 +0900

    feat(components/CalendarSection): CalendarSection init

---
+++ src/components/CalendarSection/Body/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Body = () => {
+  return (
+    <View>
+      <Text>Body</Text>
+    </View>
+  );
+};
+
+export default Body;
---
+++ src/components/CalendarSection/index.jsx
@@ -0,0 +1,14 @@
+import Header from './Header';
+import Body from './Body';
+import { View } from 'react-native';
+
+const Calendar = () => {
+  return (
+    <View>
+      <Header />
+      <Body />
+    </View>
+  );
+};
+
+export default Calendar;
