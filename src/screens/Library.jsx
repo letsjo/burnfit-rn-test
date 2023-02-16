c1fac7c4f8f65a7c360408fd193e35ba554c0c4b
letsjo <hyunoh.jo@gmail.com>
2023-02-16 23:36:41 +0900

    feat(*): screens init

---
+++ src/screens/Library.jsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const Library = () => {
+  return <Text>Library</Text>;
+};
+
+export default Library;
---
+++ src/screens/MyPage.jsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const MyPage = () => {
+  return <Text>MyPage</Text>;
+};
+
+export default MyPage;
