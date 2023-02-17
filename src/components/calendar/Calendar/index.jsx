0749666078cdd236f8ca4fada6eaa60bae874624
letsjo <hyunoh.jo@gmail.com>
2023-02-17 11:09:05 +0900

    feat(components/Calendar): header view 추가

--- src/components/calendar/Calendar/index.jsx
+++ src/components/calendar/Calendar/index.jsx
@@ -1,14 +1,22 @@
 import Header from '../Header';
 import Body from '../Body';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 const Calendar = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
       <Body />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+});
 
 export default Calendar;
--- src/components/calendar/Header/index.jsx
+++ src/components/calendar/Header/index.jsx
@@ -1,14 +1,34 @@
-import { View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 import Icon from '../../public/Icon';
 
 const Header = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableOpacity>
-        <Icon />
+        <Icon name='chevron-back-sharp' color='#3CC6F4' />
+      </TouchableOpacity>
+      <View style={styles.context}>
+        <Text>July </Text>
+        <Text>2022</Text>
+      </View>
+      <TouchableOpacity>
+        <Icon name='chevron-forward-sharp' color='#3CC6F4' />
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  context: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
 
 export default Header;
