aa877fb052fd2cb05f0e7ba89866d903b62ea434
letsjo <hyunoh.jo@gmail.com>
2023-02-17 13:54:04 +0900

    feat(utils/getMonthToString): month 영어 이름 가져오기

---
+++ src/utils/getMonthToString.jsx
@@ -0,0 +1,19 @@
+const getMonthToString = (month) => {
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return monthNames[month - 1];
+};
+
+export default getMonthToString;
