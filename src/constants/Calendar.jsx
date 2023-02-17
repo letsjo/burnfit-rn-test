d426fc693e0fe2b65ea632d9458f3e97120e8522
letsjo <hyunoh.jo@gmail.com>
2023-02-17 20:54:54 +0900

    refactor(constants/Calendar): 요일 데이터 변경

--- src/constants/Calendar.jsx
+++ src/constants/Calendar.jsx
@@ -14,11 +14,39 @@ export const MONTH_STRING = [
 ];
 
 export const DAY_STRING = [
-  'Sun',
-  'Mon',
-  'Tue',
-  'Wed',
-  'Thu',
-  'Fri',
-  'Sat',
+  {
+    id: 1,
+    name: 'Sun',
+    color: 'red',
+  },
+  {
+    id: 2,
+    name: 'Mon',
+    color: 'gray',
+  },
+  {
+    id: 3,
+    name: 'Tue',
+    color: 'gray',
+  },
+  {
+    id: 4,
+    name: 'Wed',
+    color: 'gray',
+  },
+  {
+    id: 5,
+    name: 'Thu',
+    color: 'gray',
+  },
+  {
+    id: 6,
+    name: 'Fri',
+    color: 'gray',
+  },
+  {
+    id: 7,
+    name: 'Sat',
+    color: 'blue',
+  },
 ];
