41222b09a8e93c10d22251b57ec75e23da70d779
letsjo <hyunoh.jo@gmail.com>
2023-02-17 14:45:42 +0900

    feat(*): Header props 전달

--- src/components/calendar/Calendar/index.jsx
+++ src/components/calendar/Calendar/index.jsx
@@ -1,11 +1,38 @@
-import Header from '../Header';
-import Body from '../Body';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import Header from '../Header';
+import Body from '../Body';
+import getNowDate from '../../../utils/getNowDate';
+
 const Calendar = () => {
+  const nowDate = getNowDate();
+
+  const [year, setYear] = useState(nowDate.year);
+  const [month, setMonth] = useState(nowDate.month);
+  const [date, setDate] = useState(nowDate.date);
+
+  const handlePrevMonth = () => {
+    if (month === 1) {
+      setYear(year - 1);
+      setMonth(12);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  const handleNextMonth = () => {
+    if (month === 12) {
+      setYear(year + 1);
+      setMonth(1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Header />
+      <Header year={year} month={month} />
       <Body />
     </View>
   );
--- src/components/calendar/Header/index.jsx
+++ src/components/calendar/Header/index.jsx
@@ -1,15 +1,16 @@
 import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import getMonthToString from '../../../utils/getMonthToString';
 import Icon from '../../public/Icon';
 
-const Header = () => {
+const Header = ({ year, month }) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity>
         <Icon name='chevron-back-sharp' color='#3CC6F4' />
       </TouchableOpacity>
       <View style={styles.context}>
-        <Text>July </Text>
-        <Text>2022</Text>
+        <Text>{getMonthToString(month)} </Text>
+        <Text>{year}</Text>
       </View>
       <TouchableOpacity>
         <Icon name='chevron-forward-sharp' color='#3CC6F4' />
